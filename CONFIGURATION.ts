cdaa87505043cdd488d9114840ee33cd90e45b4a
OpenTimeBrsl <136306210+OpenTimeBrsl@users.noreply.github.com>
2023-06-25 11:34:03 +0200

    Update CONFIGURATION.ts

--- CONFIGURATION.ts
+++ CONFIGURATION.ts
@@ -1,6 +1,6 @@
 export const AIRTABLE_BASE = 'appuAa65B1iKwaXXb';
 export const AIRTABLE_API_KEY = 'keysbVnYZk16tqIT0';
-export const AIRTABLE_DETAILS_FORM = 'https://airtable.com/embed/shrynWjEy8fsje5t0';
+export const AIRTABLE_DETAILS_FORM = 'https://airtable.com/shryuZ4FQQYP5VUAP';
 
 export const MAPBOX_STYLE = 'mapbox://styles/otcrew/cljb7mpjw003c01o44gwegzv9';
 export const MAPBOX_BASE_STYLE = 'mapbox://styles/otcrew/cljb7mpjw003c01o44gwegzv9';
